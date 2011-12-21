611826a3d46ac5f43ba344c1effec2d74488888b
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2011-12-21 21:58:30 +0100

    Initial import

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "config.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("TODO: implement\n");
+    printf("This is %s, version %s\n", PACKAGE, VERSION);
+    return 0;
+}
